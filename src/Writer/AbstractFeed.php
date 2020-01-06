c90f62f894d3f852c47ac2703e86590bc9f5593b
Masterflash <sxq@yandex.ru>
2020-01-06 17:31:37 +0300

    миграция на Laminas

--- src/Writer/AbstractFeed.php
+++ src/Writer/AbstractFeed.php
@@ -1,10 +1,5 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace Mf\FeedYaNews\Writer;
@@ -12,7 +7,7 @@ namespace Mf\FeedYaNews\Writer;
 use DateTime;
 use DateTimeInterface;
 use Mf\FeedYaNews\Uri;
-use Zend\Validator;
+use Laminas\Validator;
 
 class AbstractFeed
 {
